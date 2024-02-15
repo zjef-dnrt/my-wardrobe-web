b031a4e8062b40513922c8e0ea31efa93892f2c4
Tjeef <zjef96@hotmail.com>
2024-02-15 14:47:01 +0100

    feat(project): add supabase and fontawesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase"],
+  app: {
+    head: {
+      title: "My Closet",
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Raleway&display=swap",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Festive&display=swap",
+        },
+      ],
+    },
+  },
+  css: [
+    "~/assets/style/main.scss",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+  ],
+  plugins: ["~/plugins/fontawesome.ts"],
+});
---
+++ plugins/fontawesome.ts
@@ -0,0 +1,14 @@
+import { library, config } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faFacebook);
+
+// This is important, we are going to let Nuxt worry about the CSS
+config.autoAddCss = false;
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.component("font-awesome-icon", FontAwesomeIcon);
+});
