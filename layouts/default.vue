1c53b204ed450ca29cce263d70ae20298311345e
Tjeef <zjef96@hotmail.com>
2024-02-15 13:41:06 +0100

    chore(project): add tailwind config, docs and default layout

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="tw-w-screen tw-h-screen tw-bg-mistyRose-600">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,81 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  mode: "jit",
+  prefix: "tw-",
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        100: "30rem",
+      },
+      colors: {
+        mistyRose: {
+          100: "#fcf8f8",
+          200: "#f9f2f2",
+          300: "#f7ebeb",
+          400: "#f4e5e5",
+          500: "#f1dede",
+          600: "#c1b2b2",
+          700: "#918585",
+          800: "#605959",
+          900: "#302c2c",
+        },
+        helioGray: {
+          100: "#f1eef3",
+          200: "#e4dee6",
+          300: "#d6cdda",
+          400: "#c9bdcd",
+          500: "#bbacc1",
+          600: "#968a9a",
+          700: "#706774",
+          800: "#4b454d",
+          900: "#252227",
+        },
+        oldLavender: {
+          100: "#e6e3e5",
+          200: "#ccc7ca",
+          300: "#b3aab0",
+          400: "#998e95",
+          500: "#80727b",
+          600: "#665b62",
+          700: "#4d444a",
+          800: "#332e31",
+          900: "#1a1719",
+        },
+        darkPurple: {
+          100: "#d2cfd4",
+          200: "#a5a0a9",
+          300: "#77707e",
+          400: "#4a4153",
+          500: "#1d1128",
+          600: "#170e20",
+          700: "#110a18",
+          800: "#0c0710",
+          900: "#060308",
+        },
+        darkSienna: {
+          100: "#dad0d3",
+          200: "#b5a1a7",
+          300: "#91717b",
+          400: "#6c424f",
+          500: "#471323",
+          600: "#390f1c",
+          700: "#2b0b15",
+          800: "#1c080e",
+          900: "#0e0407",
+        },
+      },
+      fontFamily: {
+        raleway: "Raleway",
+        festive: "Festive",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(({ addBase, theme }) => {
+      addBase({
+        h1: { fontSize: theme("fontSize.4xl") },
+      });
+    }),
+  ],
+};
 
